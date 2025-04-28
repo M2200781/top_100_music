40195ad6e0151c747136e54f608421b6df07340e
Marcos C. Jesus <marcos.univesp2200781@gmail.com>
2025-04-28 18:29:34 -0300

    config uwsgi_params

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,11 +118,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Documentação SimpleJWT: https://django-rest-framework-simplejwt.readthedocs.io/en/latest/getting_started.html
 
